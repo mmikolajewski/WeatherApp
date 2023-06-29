b66c0b872b794ae95f4799c37f093c37ae2d69ea
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-06-29 17:16:10 +0100

    starter

---
+++ src/main/java/pl/javastart/weather/WeatherJsonApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherJsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/weather/WeatherJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
